79e56e09ae017f39e7c8e1339c8dc9329079a594
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-06-08 15:14:02 +0600

    add news

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -5,10 +5,9 @@ function Contacts() {
   return (
     <>
       <Header
-        title="Order right now!"
+        title="Order your coffee!"
         image={headerImage}>
-        he history of anime dates back to the beginning of the 20th century, when the Japanese began to show a noticeable interest in foreign techniques for creating animated films.
-
+       he history of anime dates back to the beginning of the 20th century, when the Japanese began to show a noticeable interest in foreign techniques for creating animated films.
 Despite the fact that experiments with animation were carried out in Japan before that, the first notable creation attributed to anime was the showing of "The Legend of the White Snake ", an animated film by Toei Studios . The first Otogi Manga Calendar anime series was released by Otogi Studios . It was a black and white historical cartoons. In 1963, Osamu Tezuka , nicknamed the "God of Manga", founded Mushi Productions and produced his first anime series, Tetsuwan Atom . This was the start of the anime boom .
       </Header>
     </>
