5b040b36166962f0f569005868201de8d4a07b79
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-06-08 15:06:31 +0600

    add new contacts fail

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -5,9 +5,11 @@ function Contacts() {
   return (
     <>
       <Header
-        title="Order your coffee!"
+        title="Order right now!"
         image={headerImage}>
-        The two most economically important varieties of coffee plant are the Arabica and the Robusta; ~60% of the coffee produced worldwide is Arabica and ~40% is Robusta.[5] Arabica beans consist of 0.8–1.4% caffeine and Robusta beans consist of 1.7–4.0% caffeine.
+        he history of anime dates back to the beginning of the 20th century, when the Japanese began to show a noticeable interest in foreign techniques for creating animated films.
+
+Despite the fact that experiments with animation were carried out in Japan before that, the first notable creation attributed to anime was the showing of "The Legend of the White Snake ", an animated film by Toei Studios . The first Otogi Manga Calendar anime series was released by Otogi Studios . It was a black and white historical cartoons. In 1963, Osamu Tezuka , nicknamed the "God of Manga", founded Mushi Productions and produced his first anime series, Tetsuwan Atom . This was the start of the anime boom .
       </Header>
     </>
   );
