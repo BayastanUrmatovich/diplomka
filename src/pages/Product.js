112b8e7eb74cbe08432c7a86b3021f5b251ff699
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-06-22 14:55:28 +0600

    ixmenenia

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,15 +1,14 @@
 import { useParams } from "react-router-dom";
+import CartButton from "../components/CartButton/CartButton";
 import Header from "../components/Header/Header";
 import { getProduct } from "../data/products";
 
 function Product() {
   const params = useParams();
   const product = getProduct(params.productId);
-
   if (!product) {
     return null;
   }
-
   return (
     <>
       <Header
@@ -17,8 +16,8 @@ function Product() {
         image={product.image}>
         {product.description}
       </Header>
+      <CartButton productId={product.id} />
     </>
   );
 }
-
 export default Product;
