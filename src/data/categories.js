9223cab7eee018597eae93af300da26a35dc57b5
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-06-22 15:08:06 +0600

    news izmenenia

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,24 +1,24 @@
-import beansImage from "../assets/beans.jpeg";
-import accessoriesImage from "../assets/accessories.jpeg";
-import teaImage from "../assets/tea.webp";
+import haoriImage from "../assets/haori.jpeg";
+import kunaiImage from "../assets/kunai.jpeg";
+import deathImage from "../assets/death.webp";
 
 const categories = [
   {
-    categoryId: "coffee-beans",
+    categoryId: "coffee-haori",
     title: "Haori raincoat",
-    image: beansImage,
+    image: haoriImage,
     desciption: "Haori ( Jap. 羽織) is a straight-cut Japanese jacket without buttons , worn over a kimono or with a hakama ."
   },
   {
-    categoryId: "tea",
+    categoryId: "death",
     title: "Death Note",
-    image: teaImage,
+    image: deathImage,
     desciption: "If you write the name of the victim in a notebook, he will die, you can also write how he will die."
   },
   {
-    categoryId: "accessories",
+    categoryId: "kunai",
     title: "Shuriken ",
-    image: accessoriesImage,
+    image: kunaiImage,
     desciption: "Shuriken ( Japanese 手裏剣) (literal translation: “shu” - hand, “ri” - hold, “ken” - sword) are metal arrows and knives from ten to twenty centimeters long with sharpening on both one and both ends. Shuriken is a Japanese concealed-carry throwing weapon (although sometimes used for strikes). It is a small blade made according to the type of everyday things: stars, needles, nails, knives, coins, and so on."
   },
 ];
