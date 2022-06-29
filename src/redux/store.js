4ffdcc8304cdc71e59de1abbe5fe01e7157a7c77
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-06-29 15:27:38 +0600

    checkout and store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
 import cartSlice from "./cartSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
+    auth: authSlice
   }
 });
 
@@ -11,6 +13,11 @@ store.subscribe(() => {
   localStorage.setItem('cartItems', JSON.stringify(
     store.getState().cart.items
   ));
+
+  const auth = store.getState().auth;
+  if (auth.localId !== null) {
+    localStorage.setItem('auth', JSON.stringify(auth));
+  }
 });
 
 export default store;
