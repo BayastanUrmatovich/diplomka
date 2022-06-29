9d2246909a5009af8e7736acad425a84ca9ae4c3
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-06-29 15:28:03 +0600

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,15 +6,20 @@ import { getCategories } from "../data/categories";
 function Home() {
   return (
     <>
-      <Header
-        title="Welcome to Anime store!"
-        image={headerImage}>
-       Anime (Japanese: アニメ, IPA: [aɲime] (listen)) is hand-drawn and computer-generated animation originating from Japan.Outside of Japan and in English, anime refers to Japanese animation, and refers specifically to animation produced in Japan.
-      </Header>
-
+     <Header
+title="Welcome to Anime store!"
+image={headerImage}>
+Anime (Japanese: アニメ, IPA: [aɲime] (listen)) is hand-drawn and computer-generated animation originating from Japan.Outside of Japan and in English, anime refers to Japanese animation, and refers specifically to animation produced in Japan.
+</Header>
       <CategoryList categories={getCategories()} />
     </>
   );
 }
 
 export default Home;
+
+<Header
+title="Welcome to Anime store!"
+image={headerImage}>
+Anime (Japanese: アニメ, IPA: [aɲime] (listen)) is hand-drawn and computer-generated animation originating from Japan.Outside of Japan and in English, anime refers to Japanese animation, and refers specifically to animation produced in Japan.
+</Header>
