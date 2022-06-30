f902fbdcb15f415e2751d8728d70ef3600d6a91e
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-06-30 13:52:45 +0600

    add container

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,14 @@
-import { useSelector } from "react-redux";
 import classes from "./Nav.module.css";
 import NavItem from "./NavItem/NavItem";
+import { useSelector } from "react-redux";
 
 function Nav() {
   const isAuthenticated = useSelector(store => store.auth.idToken !== null);
+  
   return (
     <ul className={classes.Nav}>
       <NavItem url="/">Home</NavItem>
-      <NavItem url="/products">All products</NavItem>
+      <NavItem url="/products">Products</NavItem>
       <NavItem url="/contacts">Contacts</NavItem>
       { isAuthenticated ? <NavItem url="/signout">Sign out</NavItem> : null }
       { !isAuthenticated ? <NavItem url="/auth">Sign in</NavItem> : null }
