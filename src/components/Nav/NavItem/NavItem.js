db7914428d4713515e85e7bf40fe4471a83e221e
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-06-30 13:53:18 +0600

    add

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -4,13 +4,14 @@ import classes from "./NavItem.module.css";
 function NavItem(props) {
   return (
     <li className={classes.NavItem}>
-      <NavLink to={props.url}
-        className={useMatch(props.url) ? classes.active : null}
-      >
+      <NavLink
+        to={props.url}
+        className={useMatch(props.url) ? classes.active : null}>
         {props.children}
+     
       </NavLink>
     </li>
   );
 }
 
-export default NavItem;
+export default NavItem; 
