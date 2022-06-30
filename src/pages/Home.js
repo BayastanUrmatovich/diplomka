e86fadf762ce0117e98f24b6c6b9b576ee78ff88
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-06-30 13:55:36 +0600

    ad container in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,9 +6,10 @@ import { getCategories } from "../data/categories";
 function Home() {
   return (
     <>
-     <Header
+     <Header 
 title="Welcome to Anime store!"
 image={headerImage}>
+  
 Anime (Japanese: アニメ, IPA: [aɲime] (listen)) is hand-drawn and computer-generated animation originating from Japan.Outside of Japan and in English, anime refers to Japanese animation, and refers specifically to animation produced in Japan.
 </Header>
       <CategoryList categories={getCategories()} />
