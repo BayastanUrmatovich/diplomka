4aa9c7ddb709b4e4b8b6ec520e728984e1c7a4b0
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-06-30 13:55:04 +0600

    products

--- src/data/products.js
+++ src/data/products.js
@@ -10,7 +10,7 @@ const products = [
     categoryId: "coffee-beans",
     title: "Kunai",
     description: `Kunai (く ない)  is a Japanese knife, possibly derived from a trowel . Two widely recognizable variations of the kunai are the small kunai and the large kunai. This simple tool could become a multifunctional weapon in the hands of a martial artist . The kunai is commonly associated with ninja , who used it to make holes in walls. The kunai was originally a farmer's tool but evolved into the weapon we know it today.`,
-    price: 10,
+    price: 100,
   },
   {
     image: productImage,
@@ -18,7 +18,7 @@ const products = [
     categoryId: "coffee-beans",
     title: "Blade Totsuka",
     description: `The Totsuka Blade (十拳 剣, Totsuka no Tsurugi ), also known as the Sakegari Long Sword (酒刈太刀, Sakegari  no Tachi ; lit. Sake Long Blade ) is a variation of the Kusanagi Blade . It is enclosed in a bottle of sake, and the blade itself is fundamentally different from any other. Instead of a physical, metallic form, the blade is essentially a liquid inside a bottle that is released and assumes that form, rather than an ordinary sword.`,
-    price: 12,
+    price: 124,
   },
   {
     image: productImage,
@@ -26,7 +26,7 @@ const products = [
     categoryId: "coffee-beans",
     title: "Tantō 短刀 Kubikiri ",
     description: `Tantō 短刀 Kubikiri  "kubikiri" translates as "Head cutter or decapitator" i.e. with a pronounced uchizori - an internal bend, the tip of the blade is rounded, blunt and retracted, not intended for stabbing, the omote side has the form of kathiri-ha zukuri, the urate side - form hira-zukuri- (flat)`,
-    price: 14,
+    price: 140,
   },
   {
     image: gariImage,
@@ -34,7 +34,7 @@ const products = [
     categoryId: "coffee-beans",
     title: "Sword of Garian",
     description: `The Garian Sword (ガリ アン刀, Garantoo ) is a three-pointed sword with a metal handle that extends into the blade. The handle controls the movements of the sword, as the blade can expand and be fired in the direction of the opponent, similar to an urumi; Kankurō compares this to the rudimentary control of puppets controlling a toy bamboo snake with chakra threads. In addition, each of the blades has the ability to respond to the wearer's chakra, amplifying it, changing its shape to that of a dragon, and manipulating it freely, similar to using a puppet technique. The sword is also used as part of the barrier for the joint technique Ninja Weapon Barrier: Reverse Fish Scale Formation .`,
-    price: 13,
+    price: 130,
   },
   {
     image: samehadaImage,
@@ -66,7 +66,7 @@ const products = [
     categoryId: "tea",
     title: "Kubikiribōchō",
     description: `The Kubikiribōchō (首斬り包丁, literally meaning: Decapitating Carving Knife) is a mystical sword renowned as the "seversword" (断刀, dantō). Like the other weapons belonging to the Seven Ninja Swordsmen of the Mist, the sword is passed down from generation to generation amongst the group's members since the First Mizukage's era.[2]`,
-    price: 10,
+    price: 1009,
   },
 ];
 
